55eb271b4db70deccdd72f4ecdfe7b775adf7788
Joon14045 <32318030+Joon14045@users.noreply.github.com>
2017-09-27 10:23:29 +0700

    Update MainClassJoon.java

--- MainClassJoon.java
+++ MainClassJoon.java
@@ -1,8 +1,80 @@
-package com.company;
-
 public class MainClassJoon {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+
+        System.out.println("Hello world.");
+
+        int i = 0;
+        while(i < 100){
+            System.out.println("Hello world.");
+            i++;
+        }
+
+        System.out.println("---------------");
+
+
+        for(int k = 0l k < 100; k++){
+            System.out.println("Hello world.");
+        }
+
+
+        System.out.println("---------------");
+        for(int k = 10; k < 61; k+=2){
+            System.out.println(k);
+        }
+
+        System.out.println("-------------");
+
+        for(int k= 15; k > -36; k-=5){
+            System.out.println(k);
+        }
+
+        System.out.println("--------------------");
+
+        int [] myIntArray = new int[5];
+
+
+        myIntArray[0] = 23;
+        myIntArray[1] = 34;
+        myIntArray[2] = 99;
+        myIntArray[4] = 1;
+
+        System.out.println("---------------");
+
+        byte b; //8 bits
+        short s; //16 bits
+        int i1; //32 bits
+        long l; //64 bits
+
+
+        float f; //32 bits
+        double d; //64 bits
+
+
+    boolean Rainforest = false;
+
+
+
+
+
+
+    //write a conditional in which if isRaining is true,
+    //print out "Take your rain coat with you.",
+    //and if the isRaining variable is false, print out "No need for a raincoat".
+
+
+
+    //Declare a Scanner object called sc. (Be sure it is imported properly....)
+    //Ask the user the grade they got on the test (0 - 100)
+    //Assign that to an int variable called score.
+
+
+    //Now, write a conditional block in which if the score is
+    //over 89, output A
+    //else if over 79, output B
+    //else if over 69, output C
+    //else output below C
+
+
+
 }
